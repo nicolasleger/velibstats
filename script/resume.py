5c01b239c05136e364632c8c396c8c4d5e6b951f
JonathanMM <jonathanmm@free.fr>
2018-01-12 20:35:35 +0100

    Ajustement du calcul du resume au début de période

--- script/resume.py
+++ script/resume.py
@@ -5,7 +5,8 @@ dureeConso = 5 #En minutes
 #for i in range(0, 11):
     #dateConsoDT = datetime.datetime(2018,1,9,11,i * dureeConso,0)
 dateConsoDT = datetime.datetime.now() - datetime.timedelta(minutes=dureeConso)
-dateConsoDT = dateConsoDT.replace(second = 0)
+debutPeriodeMinute = dateConsoDT.minute % dureeConso
+dateConsoDT = dateConsoDT.replace(microsecond = 0, second = 0, minute=dateConsoDT.minute - debutPeriodeMinute)
 calculerResume(dateConsoDT, dureeConso)
 
 # dateConsoDT = datetime.datetime(2018,1,11,18,15,0)
